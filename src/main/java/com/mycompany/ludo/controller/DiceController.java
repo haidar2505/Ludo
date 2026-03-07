eb667d4a95a2db0ead3cc4fe5da9b23d6beb51a1
haidar2505 <haidar.ameermeea@gmail.com>
2026-03-07 09:21:08 +0400

    Added controller

--- src/main/java/com/mycompany/ludo/controller/DiceController.java
+++ src/main/java/com/mycompany/ludo/controller/DiceController.java
@@ -2,12 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.ludo.logic;
+package com.mycompany.ludo.controller;
+
+import com.mycompany.ludo.model.Dice;
+import java.util.Random;
 
 /**
  *
  * @author Haidar
  */
-public class Dice {
-    
+public class DiceController {
+
+    Dice dice = new Dice();
+    Random random = new Random();
+
+    public int rollDice() {
+        int min = 1;
+        int max = 6;
+        int result = random.nextInt(max - min + 1) + min;
+        dice.setNumberRolled(result);
+        return result;
+    }
 }
---
+++ src/main/java/com/mycompany/ludo/model/Dice.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ludo.model;
+
+/**
+ *
+ * @author Haidar
+ */
+public class Dice {
+
+    private int numberRolled;
+
+    public Dice() {
+    }
+
+    public int getNumberRolled() {
+        return numberRolled;
+    }
+
+    public void setNumberRolled(int numberRolled) {
+        this.numberRolled = numberRolled;
+    }
+}
