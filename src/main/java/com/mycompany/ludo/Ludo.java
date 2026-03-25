bdafbff3bc01657dc5900cce56b7f726cff27191
haidar2505 <haidar.ameermeea@gmail.com>
2026-03-25 09:39:05 +0400

    First commit

---
+++ src/main/java/com/mycompany/ludo/Ludo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ludo;
+
+/**
+ *
+ * @author Haidar
+ */
+public class Ludo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
